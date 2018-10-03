c1fcdb92f9d3501793342634d9dfcf7f26663ed3
Unbelievable <slavlen1999@gmail.com>
2018-10-03 23:35:36 +0300

    only 79%, maybe will find a better solution and fix later

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  var multipliers = getMultipliers(number, base);
+  function getMultipliers(number, base) {
+    var multipliers = [];
+    if (base > 1) { 
+      for (var i = 2; i <= base; i++) { 
+        while (base % i == 0 && base > 1) { 
+          multipliers.push(i); 
+          base = base/i; 
+        } 
+        if (base/i == 1) { 
+          multipliers.push(base); 
+        } 
+      } 
+    }  
+    return multipliers;
+  }
+  var zerosArray = [];
+  for (var i = 0; i < multipliers.length; i++) {
+    var currentMultiplier = multipliers[i];
+    var components = 0;
+    for (var n = 1; Math.pow(currentMultiplier, n) <= number; n++) { 
+      components += Math.floor(number/Math.pow(currentMultiplier, n));  
+    } 
+    zerosArray.push(components);
+  }
+  var zerosTotal = Math.min(...zerosArray); 
+  return zerosTotal; 
 }
